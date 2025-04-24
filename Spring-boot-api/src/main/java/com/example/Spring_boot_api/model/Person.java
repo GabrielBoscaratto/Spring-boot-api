2371711248b8e1e87e4855fa55f2711a80281dbd
Gabriel Boscaratto <104937145+GabrielBoscaratto@users.noreply.github.com>
2025-04-24 18:38:20 -0300

    feat: adiciona endpoint de criação de usuário

--- Spring-boot-api/src/main/java/com/example/Spring_boot_api/model/Person.java
+++ Spring-boot-api/src/main/java/com/example/Spring_boot_api/model/Person.java
@@ -1,26 +1,38 @@
 package com.example.Spring_boot_api.model;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Person implements Serializable{
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
-    public Person() {
-    }
+    public Person() {}
 
     public Long getId() {
         return id;
     }
 
-    public static void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -28,7 +40,7 @@ public class Person implements Serializable{
         return firstName;
     }
 
-    public static void setFirstName(String firstName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
@@ -36,7 +48,7 @@ public class Person implements Serializable{
         return lastName;
     }
 
-    public static void setLastName(String lastName) {
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
@@ -44,7 +56,7 @@ public class Person implements Serializable{
         return address;
     }
 
-    public static void setAddress(String address) {
+    public void setAddress(String address) {
         this.address = address;
     }
 
@@ -57,51 +69,19 @@ public class Person implements Serializable{
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+               Objects.equals(firstName, person.firstName) &&
+               Objects.equals(lastName, person.lastName) &&
+               Objects.equals(address, person.address) &&
+               Objects.equals(gender, person.gender);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Person other = (Person) obj;
-        if (address == null) {
-            if (other.address != null)
-                return false;
-        } else if (!address.equals(other.address))
-            return false;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
-        if (gender == null) {
-            if (other.gender != null)
-                return false;
-        } else if (!gender.equals(other.gender))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
     }
 }
